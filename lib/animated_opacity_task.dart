2bc4a5e93b538871a87aaabdec2f806e86552b6e
nikunj11sanghani <nikunj.sanghani@mail.codetrade.io>
2023-08-01 16:39:47 +0530

    [Add]AutoSizeText Package

--- lib/animated_opacity_task.dart
+++ lib/animated_opacity_task.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class AnimatedOpacityTask extends StatefulWidget {
@@ -38,7 +39,6 @@ class _AnimatedOpacityState extends State<AnimatedOpacityTask> {
                   });
                 },
                 child: const Text("Change Opacity")),
-
             AnimatedPadding(
               padding: EdgeInsets.all(paddingVal),
               duration: const Duration(seconds: 3),
@@ -56,10 +56,8 @@ class _AnimatedOpacityState extends State<AnimatedOpacityTask> {
                 },
                 child: const Text("Change Padding")),
             AnimatedSwitcher(
-
               duration: const Duration(milliseconds: 500),
-              child: Text("$count",key: ValueKey(count)),
-
+              child: Text("$count", key: ValueKey(count)),
             ),
             OutlinedButton(
                 onPressed: () {
@@ -68,6 +66,24 @@ class _AnimatedOpacityState extends State<AnimatedOpacityTask> {
                   });
                 },
                 child: const Text("Switcher")),
+            const AutoSizeText(
+              "Because Row and other widgets like Container,"
+              " Column or ListView do not constrain their children,"
+              " the text will overflowYou can fix this by constraining the AutoSizeText."
+              " Wrap it with Expanded in case of Row and Column or use a SizedBox or another "
+              "widget with fixed width (and height).",
+              maxLines: 2,
+              minFontSize: 10,
+              style: TextStyle(fontSize: 20),
+              overflow: TextOverflow.ellipsis,
+              overflowReplacement: Text("Not Suitable"),
+              //overflowReplacement what displayed if it has overflow issue
+              stepGranularity: 5,
+              //stepGranularity means if over flow happens then it's next font size decrease by 5 like
+              presetFontSizes: [20, 35, 30],
+              // presetFontSizes means if over flow happens then it will first got 40 next one is 35 and after this 30
+              //and if it is given then all font sizes are ignored
+            )
           ],
         ),
       ),
